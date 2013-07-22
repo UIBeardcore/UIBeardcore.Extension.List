866b80cc24c26a4317a2dce897f906535084c09d
Oleksandr Orlov <oorlov@sdl.com>
2013-07-22 13:02:33 +0300

    Added List Definition Handler
    Added List Definition Handler

---
+++ Scripts/ListExtensions/ListDefinitionHandlers.js
@@ -0,0 +1,14 @@
+﻿/**
+ * Namespace for Definition Handlers functions.
+ */
+Type.registerNamespace("UIBeardcore.Editors.ListExtensions.DefinitionHandlers");
+
+UIBeardcore.Editors.ListExtensions.DefinitionHandlers.HandleExtensionItemDefinition = function UIBeardcore$Translators$HandleExtensionItemDefinition(editorName, view, controlId, itemId)
+{
+	if ($models.getItemType(itemId) == $const.ItemType.FOLDER)
+	{
+		return $config.expandEditorPath("/Xml/Definitions/HandlerListDefinition.xml", editorName);
+	}
+	
+	return $config.expandEditorPath("/Xml/Definitions/HandlerListDefinition.xml", editorName);
+};
